e712474d1b0cc263277238a3c4da05a10594fbb2
mirak <karmau473@gmail.com>
2025-01-21 15:56:29 +0300

    refinement

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
-	"log"
 	"music-library/config"
 	"music-library/controllers"
 	"music-library/database"
+	"music-library/repository"
+	"music-library/utils"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+var log = logrus.New()
+
+func initLog(level string) {
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	log.SetOutput(os.Stdout)
+
+	switch level {
+	case "debug":
+		log.SetLevel(logrus.DebugLevel)
+	case "info":
+		log.SetLevel(logrus.InfoLevel)
+	case "warn":
+		log.SetLevel(logrus.WarnLevel)
+	case "error":
+		log.SetLevel(logrus.ErrorLevel)
+	default:
+		log.SetLevel(logrus.InfoLevel)
+	}
+}
 
 // @title Music Library API
 // @version 1.0
@@ -18,31 +43,49 @@ import (
 // @host localhost:5051
 // @BasePath /
 func main() {
-	_ = config.LoadEnv()
-
-	dbInstance := database.NewDatabase()
-	err := dbInstance.Connect()
+	cfg, err := config.LoadEnv()
 	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
+		log.WithError(err).Fatal("Failed to load environment variables")
 	}
 
-	log.Println("INFO: Database connected successfully.")
+	initLog(cfg.LOG_LEVEL)
+
+	dbInstance := database.NewDatabase()
+	err = dbInstance.Connect()
+	if err != nil {
+		log.WithError(err).Fatal("Failde to connect to database")
+	}
+
+	log.Info("Database connected successfully.")
 
 	db := dbInstance.GetDB()
 
 	database.Migrate(db)
-	log.Println("INFO: Database migrations completed.")
+	log.Info("Database migrations completed.")
+
+	songRep := repository.NewSongRepository(db)
 
 	r := gin.Default()
 
 	r.GET("/info", controllers.GetSongInfo)
-	r.GET("/songs", controllers.GetSongs)
+	r.GET("/songs", func(c *gin.Context) {
+		page := c.DefaultQuery("page", "1")
+		limit := c.DefaultQuery("limit", "10")
+		songs, err := songRep.GetAllSongs(utils.ToInt(page), utils.ToInt(limit))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, songs)
+	})
+	r.POST("/songs", controllers.CreateSong)
 	r.GET("/song/:id/verses", controllers.GetSongTextWithPagination)
 	r.PUT("/song/:id", controllers.UpdateSong)
+	r.PATCH("/song/:id", controllers.PartialUpdateSong)
 	r.DELETE("/song/:id", controllers.DeleteSong)
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	log.Println("INFO: Swagger documentation available at http://localhost:5050/swagger/index.html")
+	log.Info("Swagger documentation available at http://localhost:5050/swagger/index.html")
 
 	go func() {
 		testRouter := gin.Default()
@@ -68,11 +111,13 @@ func main() {
 			c.JSON(http.StatusOK, songDetail)
 		})
 
-		if err := testRouter.Run(":5051"); err != nil {
-			log.Fatalf("ERROR: Failed to start the test server: %v", err)
+		if err := testRouter.Run(cfg.TEST_SERVER_ADDRESS); err != nil {
+			log.WithError(err).Fatal("Failed to start the test server")
 		}
 	}()
 
-	log.Panicln("INFO: Starting the main server on port 5050")
-	log.Fatal(r.Run(":5050"))
+	log.Info("Starting the main server on port 5050")
+	if err := r.Run(cfg.SERVER_ADDRESS); err != nil {
+		log.WithError(err).Fatal("Failed to start the main server")
+	}
 }
--- config/config.go
+++ config/config.go
@@ -8,30 +8,37 @@ import (
 )
 
 type Config struct {
-	DBHost           string
-	DBPort           string
-	DBUser           string
-	DBPassword       string
-	DBName           string
-	DBURL            string
-	EXTERNAL_API_URL string
-	LOG_LEVEL        string
+	// Database
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBURL      string
+
+	// Server
+	SERVER_ADDRESS      string
+	TEST_SERVER_ADDRESS string
+	EXTERNAL_API_URL    string
+	LOG_LEVEL           string
 }
 
-func LoadEnv() *Config {
+func LoadEnv() (*Config, error) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Failed loading .env file: %v", err)
 	}
 
 	return &Config{
-		DBHost:           os.Getenv("DB_HOST"),
-		DBPort:           os.Getenv("DB_PORT"),
-		DBUser:           os.Getenv("DB_USER"),
-		DBPassword:       os.Getenv("DB_PASSWORD"),
-		DBName:           os.Getenv("DB_NAME"),
-		DBURL:            os.Getenv("DATABASE_URL"),
-		EXTERNAL_API_URL: os.Getenv("EXTERNAL_API_URL"),
-		LOG_LEVEL:        os.Getenv("LOG_LEVEL"),
-	}
+		DBHost:              os.Getenv("DB_HOST"),
+		DBPort:              os.Getenv("DB_PORT"),
+		DBUser:              os.Getenv("DB_USER"),
+		DBPassword:          os.Getenv("DB_PASSWORD"),
+		DBName:              os.Getenv("DB_NAME"),
+		DBURL:               os.Getenv("DATABASE_URL"),
+		SERVER_ADDRESS:      os.Getenv("SERVER_ADDRESS"),
+		TEST_SERVER_ADDRESS: os.Getenv("TEST_SERVER_ADDRESS"),
+		EXTERNAL_API_URL:    os.Getenv("EXTERNAL_API_URL"),
+		LOG_LEVEL:           os.Getenv("LOG_LEVEL"),
+	}, nil
 }
